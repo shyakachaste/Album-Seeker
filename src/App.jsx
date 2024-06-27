9a69a5141d3e967391c9512dd7b71085f1b43adc
Shyaka Chaste <liochastej@gmail.com>
2024-06-27 16:28:23 -0700

    starting

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css';
+import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
+function App() {
+  
+
+  return (
+    <Container>
+  <InputGroup>
+    <FormControl
+      placeholder="Search For Artist"
+      type="input"
+      aria-label="Search for an Artist"
+      onKeyDown={} // search function
+      onChange={} // setSearch
+      style={{
+        width: "300px",
+        height: "35px",
+        borderWidth: "0px",
+        borderStyle: "solid",
+        borderRadius: "5px",
+        marginRight: "10px",
+        paddingLeft: "10px",
+      }}
+    />
+
+    <Button onClick={{}}>Search</Button>
+  </InputGroup>
+</Container>
+  )
+}
+export default App
