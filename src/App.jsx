fee02c74900b56ae89fe7d2d32d256511375b6c0
Shyaka Chaste <liochastej@gmail.com>
2024-06-28 03:35:22 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,128 @@
-import './App.css';
-import { FormControl, InputGroup, Container, Button } from "react-bootstrap";
+import "./App.css";
+import {
+  FormControl,
+  InputGroup,
+  Container,
+  Button,
+  Card,
+  Row,
+} from "react-bootstrap";
+import { useState, useEffect } from "react";
+
 const clientId = import.meta.env.VITE_CLIENT_ID;
 const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
 
 function App() {
-  
+  const [searchInput, setSearchInput] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    let authParams = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body:
+        "grant_type=client_credentials&client_id=" +
+        clientId +
+        "&client_secret=" +
+        clientSecret,
+    };
+
+    fetch("https://accounts.spotify.com/api/token", authParams)
+      .then((result) => result.json())
+      .then((data) => {
+        setAccessToken(data.access_token);
+      });
+  }, []);
+
+  async function search() {
+    let artistParams = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    };
+
+    // Get Artist
+    const artistID = await fetch(
+      "https://api.spotify.com/v1/search?q=" + searchInput + "&type=artist",
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        return data.artists.items[0].id;
+      });
+
+    // Get Artist Albums
+    await fetch(
+      "https://api.spotify.com/v1/artists/" +
+        artistID +
+        "/albums?include_groups=album&market=US&limit=50",
+      artistParams
+    )
+      .then((result) => result.json())
+      .then((data) => {
+        setAlbums(data.items);
+      });
+  }
 
   return (
-    <Container>
-  <InputGroup>
-    <FormControl
-      placeholder="Search For Artist"
-      type="input"
-      aria-label="Search for an Artist"
-      onKeyDown={} // search function
-      onChange={} // setSearch
-      style={{
-        width: "300px",
-        height: "35px",
-        borderWidth: "0px",
-        borderStyle: "solid",
-        borderRadius: "5px",
-        marginRight: "10px",
-        paddingLeft: "10px",
-      }}
-    />
+    <>
+      <Container className="container">
+        <InputGroup className="search-bar">
+          <FormControl
+            placeholder="Search For Artist"
+            type="text"
+            aria-label="Search for an Artist"
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                search();
+              }
+            }}
+            onChange={(event) => setSearchInput(event.target.value)}
+            className="search-input"
+          />
+          <Button onClick={search} className="buttonsearch">Search</Button>
+        </InputGroup>
+      </Container>
 
-    <Button onClick={{}}>Search</Button>
-  </InputGroup>
-</Container>
-  )
+      <Container>
+        <Row className="album-row">
+          {albums.map((album) => {
+            return (
+              <Card
+                key={album.id}
+                className="album-card"
+              >
+                <Card.Img
+                  width={200}
+                  src={album.images[0].url}
+                  className="album-image"
+                />
+                <Card.Body>
+                  <Card.Title className="album-title">
+                    {album.name}
+                  </Card.Title>
+                  <Card.Text className="album-text">
+                    Release Date: <br /> {album.release_date}
+                  </Card.Text>
+                  <Button
+                    href={album.external_urls.spotify}
+                    className="album-link"
+                  >
+                    Album Link
+                  </Button>
+                </Card.Body>
+              </Card>
+            );
+          })}
+        </Row>
+      </Container>
+    </>
+  );
 }
-export default App
+
+export default App;
